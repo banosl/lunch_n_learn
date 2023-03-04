5755604dca9701130927b392dd16adc783c7882f
Leo Banos Garcia <bsleo625@gmail.com>
2023-03-04 11:43:45 -0700

    Adds get recipes by country

--- app/services/recipes_service.rb
+++ app/services/recipes_service.rb
@@ -1,3 +1,16 @@
 class RecipesService
-  
+  def self.conn
+    Faraday.new(url: "https://api.edamam.com/api/",
+                params: {app_id: ENV["app_id"], app_key: ENV["app_key"]},
+                headers: {'Content-Type' => 'application/json'})
+  end
+
+  def json_parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.get_recipes_by_country(country)
+    response = conn.get('recipes/v2', {type: "public", q: country})
+    binding.pry
+  end
 end
--- spec/services/recipes_service_spec.rb
+++ spec/services/recipes_service_spec.rb
@@ -4,6 +4,17 @@ RSpec.describe RecipesService do
   describe '#get_recipes_by_country' do
     it "returns json with a list of recipes from the country queried" do
       country = "Mexico"
+
+      stub_request(:get, "https://api.edamam.com/api/recipes/v2?app_id=979855b9&app_key=29f347f65a3d8f279cd071f1f01a3096&q=Mexico&type=public").
+      with(
+        headers: {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'Content-Type'=>'application/json',
+        'User-Agent'=>'Faraday v2.7.4'
+        }).
+      to_return(status: 200, body: File.read("spec/fixtures/recipes_mexico.json"))
+
       response = RecipesService.get_recipes_by_country(country)
 
       expect(response).to be(Hash)

fd72429e88676f76bf881ca4faee83d777d42900
Leo Banos Garcia <bsleo625@gmail.com>
2023-03-05 12:27:03 -0700

    Adds test for when a random country is given

--- spec/requests/api/v1/recipes/index_spec.rb
+++ spec/requests/api/v1/recipes/index_spec.rb
@@ -35,7 +35,34 @@ RSpec.describe "GET recipes" do
     end
   end
 
-  it "can return recipes for a random country when not given a country query"
+  it "can return recipes for a random country when not given a country query" do
+    country = ""
+    get "/api/v1/recipes?country=#{country}"
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    
+    recipes = JSON.parse(response.body, symbolize_names: true)
+
+    expect(recipes).to have_key(:data)
+    expect(recipes[:data]).to be_a(Array)
+    
+    recipes[:data].each do |recipe|
+      expect(recipe).to have_key(:id)
+      expect(recipe[:id]).to eq({})
+      expect(recipe).to have_key(:type)
+      expect(recipe[:type]).to eq("recipe")
+      expect(recipe).to have_key(:attributes)
+      expect(recipe[:attributes]).to have_key(:title)
+      expect(recipe[:attributes][:title]).to be_a(String)
+      expect(recipe[:attributes]).to have_key(:url)
+      expect(recipe[:attributes][:url]).to be_a(String)
+      expect(recipe[:attributes]).to have_key(:country)
+      expect(recipe[:attributes][:country]).to be_a(String)
+      expect(recipe[:attributes]).to have_key(:image)
+      expect(recipe[:attributes][:image]).to be_a(String)
+    end
+  end
 
   it "can return an empty data array when there are no results"
 end

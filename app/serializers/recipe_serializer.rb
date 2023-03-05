767ff537bb2d31e1ed12e144bdbfcc0adce81314
Leo Banos Garcia <bsleo625@gmail.com>
2023-03-05 12:20:29 -0700

    Adds more tests for recipe serializer

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,21 +1,20 @@
 class RecipeSerializer
   def self.format_country_recipes(recipes)
-    binding.pry
-    recipes.each do |recipe|
       {
         "data": 
-        [
-          "id":{},
-          "type": "recipe",
-          "attributes":
-          { 
-              "title": recipe[:recipe][:name],
-              "url": recipe[:recipe][:url],
-              "country": recipe[:recipe][:country],
-              "image": recipe[:recipe][:image]
+          recipes.map do |recipe|
+            {
+              "id": {},
+              "type": "recipe",
+              "attributes":
+              { 
+                "title": recipe[:recipe][:name],
+                "url": recipe[:recipe][:url],
+                "country": recipe[:recipe][:country],
+                "image": recipe[:recipe][:image]
+              }
             }
-        ]
+          end
       }
-    end
   end
 end

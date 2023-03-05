7ffa8f84a346a9473889b23755bb35252699bf3d
Leo Banos Garcia <bsleo625@gmail.com>
2023-03-04 21:14:15 -0700

    Removes coverage and adds it to ignore

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::RecipesController < ApplicationController
   def index
     recipes = RecipesFacade.recipes_by_country(params[:country])
     binding.pry
-    render json: RecipeSerializer.format_country_recipes(recipes)
+    render json: RecipeSerializer.format_country_recipes(recipes), status: 200
   end
 end

060a9d47f0d865e61bb0f9401ff4fa0daa970e27
Leo Banos Garcia <bsleo625@gmail.com>
2023-03-05 16:53:19 -0700

    Adds pictures serializer

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::LearningResourcesController < ApplicationController
   def index
     country = params[:country]
     video = YouTubeFacade.one_video_for_country(country)
-binding.pry
+    pictures = PicturesFacade.pictures_for_country(country)
+# binding.pry
+    render json: LearningResourcesSerializer.format_video_and_pictures(country, video, pictures), status: 200
   end
 end
--- spec/requests/api/v1/learning_resources/index_spec.rb
+++ spec/requests/api/v1/learning_resources/index_spec.rb
@@ -1,11 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'GET learning resources' do
+  before :each do
+    load_stubs
+  end
   it "can return a video and 10 images for the given country in the response" do
     country = "Mexico"
     get "/api/v1/learning_resources?country=#{country}"
 
     expect(response).to be_successful
     expect(response.status).to eq(200)
+
+    result = JSON.parse(response.body, symbolize_names: true)
+
+    expect(result[:data]).to have_key(:type)
+    expect(result[:data]).to have_key(:attributes)
+    expect(result[:data][:attributes]).to have_key(:country)
+    expect(result[:data][:attributes][:country]).to eq(country)
+    expect(result[:data][:attributes]).to have_key(:video)
+    expect(result[:data][:attributes][:video]).to have_key(:title)
+    expect(result[:data][:attributes][:video][:title]).to eq("A Super Quick History of Mexico")
+    expect(result[:data][:attributes][:video]).to have_key(:youtube_video_id)
+    expect(result[:data][:attributes]).to have_key(:images)
+    expect(result[:data][:attributes][:images]).to be_a(Array)
   end
 end

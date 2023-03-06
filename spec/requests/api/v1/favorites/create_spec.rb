c58b1b3f31591d0882067da7f77c91ebbdba33dc
Leo Banos Garcia <bsleo625@gmail.com>
2023-03-05 18:43:14 -0700

    removes keys

---
+++ spec/requests/api/v1/favorites/create_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe "Create a favorite" do
+  describe "user can create a favorite from a post request" do
+    it "when receiving a known api_key the favorite gets added to the user" do
+
+    end
+
+    it "when the api_key is not know they get a message that says that user doesn't exist with a 400 level status code"
+  end
+end
--- spec/web_stubs.rb
+++ spec/web_stubs.rb
@@ -7,26 +7,26 @@ def load_stubs
 end
 
 def recipes_mexico
-  stub_request(:get, "https://api.edamam.com/api/recipes/v2?app_id=979855b9&app_key=29f347f65a3d8f279cd071f1f01a3096&q=Mexico&type=public").
+  stub_request(:get, "https://api.edamam.com/api/recipes/v2").
       with(
         headers: {
         'Accept'=>'*/*',
         'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
         'Content-Type'=>'application/json',
         'User-Agent'=>'Faraday v2.7.4'
-        }).
+        }, query: {"app_id": ENV['app_id'], "app_key": ENV['app_key'], "q": "Mexico", "type": "public"}).
       to_return(status: 200, body: File.read("spec/fixtures/recipes_mexico.json"))
 end
 
 def recipes_empty
-  stub_request(:get, "https://api.edamam.com/api/recipes/v2?app_id=979855b9&app_key=29f347f65a3d8f279cd071f1f01a3096&q=Tokelau&type=public").
+  stub_request(:get, "https://api.edamam.com/api/recipes/v2").
          with(
            headers: {
        	  'Accept'=>'*/*',
        	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
        	  'Content-Type'=>'application/json',
        	  'User-Agent'=>'Faraday v2.7.4'
-           }).
+           }, query: {"app_id": ENV['app_id'], "app_key": ENV['app_key'], "q": "Tokelau", "type": "public"}).
          to_return(status: 200, body: File.read("spec/fixtures/recipes_empty.json"))
 end
 
@@ -43,14 +43,14 @@ def all_countries
 end
 
 def youtube_mexico
-  stub_request(:get, "https://youtube.googleapis.com/youtube/v3/search?channelId=UCluQ5yInbeAkkeCndNnUhpw&key=AIzaSyA1cabsfcfS4Bo_39XsvGwDClx3JRrvIPM&part=snippet&q=Mexico&type=video").
+  stub_request(:get, "https://youtube.googleapis.com/youtube/v3/search").
          with(
            headers: {
        	  'Accept'=>'*/*',
        	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
        	  'Content-Type'=>'application/json',
        	  'User-Agent'=>'Faraday v2.7.4'
-           }).
+           }, query: {"key": ENV['GOOGLE_API'], "part": "snippet", "q": "Mexico", "type": "video"}).
          to_return(status: 200, body: File.read("spec/fixtures/youtube_mexico.json"))
 end
 
@@ -62,6 +62,6 @@ def pictures_mexico
        	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
        	  'Accept-Version'=>'v1',
        	  'User-Agent'=>'Faraday v2.7.4'
-           }).
+           }, query: {"client_id": ENV['unsplash_api'], "query": "Mexico"}).
          to_return(status: 200, body: File.read("spec/fixtures/pictures_mexico.json"))
 end

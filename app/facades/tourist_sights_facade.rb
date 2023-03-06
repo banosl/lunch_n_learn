a6d4ad5bdd644f528588d5bfb439e2da304bddc0
Leo Banos Garcia <bsleo625@gmail.com>
2023-03-06 10:45:02 -0700

    Adds tourist_sights_facade with #sights_for_country_capital

--- app/facades/tourist_sights_facade.rb
+++ app/facades/tourist_sights_facade.rb
@@ -1,5 +1,19 @@
 class TouristSightsFacade
   def self.sights_for_country_capital(country)
+    capital_info = CountriesService.get_one_country(country)
+    coordinates = capital_info[0][:capitalInfo][:latlng].rotate.join(",")
+    capital_sights = PlacesService.get_tourist_sights_of_capital(coordinates)
     
+    {
+      country: capital_info[0][:name],
+      capital_city: capital_info[0][:capital][0],
+      sights: capital_sights[:features].map do |sight|
+        {
+          name: sight[:properties][:name],
+          address: sight[:properties][:formatted],
+          place_id: sight[:properties][:place_id]
+        }
+      end
+      }
   end
 end
--- spec/facades/tourist_sights_facade_spec.rb
+++ spec/facades/tourist_sights_facade_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe TouristSightsFacade do
+  before :each do
+    load_stubs
+  end
   describe "#sights_for_country_capital" do
     it "returns a country's capital and sights (name, formatted address, place_id)" do
       country = "Mexico"
@@ -8,7 +11,21 @@ RSpec.describe TouristSightsFacade do
       capital_sights = TouristSightsFacade.sights_for_country_capital(country)
 
       expect(capital_sights).to be_a(Hash)
-      binding.pry
+    
+      expect(capital_sights).to have_key(:country)
+      expect(capital_sights).to have_key(:capital_city)
+      expect(capital_sights).to have_key(:sights)
+
+      capital_sights[:sights].each do |sight|
+        expect(sight).to have_key(:name)
+        expect(sight[:name]).to be_a(String)
+        expect(sight).to have_key(:address)
+        expect(sight[:address]).to be_a(String)
+        expect(sight).to have_key(:place_id)
+        expect(sight[:place_id]).to be_a(String)
+      end
+
+      expect(capital_sights[:sights][0][:name]).to eq("Biblioteca de México \"José Vasconcelos\"")
     end
   end
 end

dba0f595b573b10aa4e2f5c6e3ca313ae821ff4a
Leo Banos Garcia <bsleo625@gmail.com>
2023-03-06 11:39:45 -0700

    Adds tourist_sight poro

--- app/facades/tourist_sights_facade.rb
+++ app/facades/tourist_sights_facade.rb
@@ -3,6 +3,8 @@ class TouristSightsFacade
     capital_info = CountriesService.get_one_country(country)
     coordinates = capital_info[0][:capitalInfo][:latlng].rotate.join(",")
     capital_sights = PlacesService.get_tourist_sights_of_capital(coordinates)
+
+    binding.pry
     {
       country: capital_info[0][:name],
       capital_city: capital_info[0][:capital][0],

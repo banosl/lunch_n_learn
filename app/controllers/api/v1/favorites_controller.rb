c9126d522f7e6f48c998fabf03920317fdd214d4
Leo Banos Garcia <bsleo625@gmail.com>
2023-03-05 19:40:49 -0700

    Adds create for favorite

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,5 +1,18 @@
 class Api::V1::FavoritesController < ApplicationController
   def create
     binding.pry
+    if User.find_by(api_key: params[:api_key])
+      user = User.find_by(api_key: params[:api_key])
+      new_favorite = user.favorites.new(favorite_params)
+      
+      if new_favorite.save
+        render json: {"success": "Favorite added successfully"}, status: 200
+      end
+    end
+  end
+
+  private
+  def favorite_params
+    params.permit(:country, :recipe_link, :recipe_title)
   end
 end
--- spec/requests/api/v1/favorites/create_spec.rb
+++ spec/requests/api/v1/favorites/create_spec.rb
@@ -7,7 +7,12 @@ RSpec.describe "Create a favorite" do
       headers = { "CONTENT_TYPE" => "application/json" }
 
       post "/api/v1/favorites", headers: headers, params: JSON.generate({api_key: "6816bbb16b76946463be", country: "Thailand", recipe_link: "https://www.thaifood.com", recipe_title: "Crab Fried Rice"})
-      binding.pry
+      
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      
+      result = JSON.parse(response.body, symbolize_names: true)
+      expect(result).to eq({success: "Favorite added successfully"})
     end
 
     it "when the api_key is not know they get a message that says that user doesn't exist with a 400 level status code"

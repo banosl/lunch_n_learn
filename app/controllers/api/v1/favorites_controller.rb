e04837f1a1b44148e016fc82d5e3a1bb33d1d377
Leo Banos Garcia <bsleo625@gmail.com>
2023-03-05 19:16:24 -0700

    Adds favorites controller

---
+++ app/controllers/api/v1/favorites_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::FavoritesController < ApplicationController
+  def create
+    binding.pry
+  end
+end
--- spec/requests/api/v1/favorites/create_spec.rb
+++ spec/requests/api/v1/favorites/create_spec.rb
@@ -3,7 +3,11 @@ require "rails_helper"
 RSpec.describe "Create a favorite" do
   describe "user can create a favorite from a post request" do
     it "when receiving a known api_key the favorite gets added to the user" do
+      user = User.create({name: "bob", email: "bob@bob.com", api_key: "6816bbb16b76946463be"})
+      headers = { "CONTENT_TYPE" => "application/json" }
 
+      post "/api/v1/favorites", headers: headers, params: JSON.generate({api_key: "6816bbb16b76946463be", country: "Thailand", recipe_link: "https://www.thaifood.com", recipe_title: "Crab Fried Rice"})
+      binding.pry
     end
 
     it "when the api_key is not know they get a message that says that user doesn't exist with a 400 level status code"

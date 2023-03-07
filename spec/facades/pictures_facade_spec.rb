9eba0fb1f0e88a69583b31843421a8ef588063b6
Leo Banos Garcia <bsleo625@gmail.com>
2023-03-06 18:57:40 -0700

    Fix test

--- spec/facades/pictures_facade_spec.rb
+++ spec/facades/pictures_facade_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe PicturesFacade do
       end
 
       expect(pictures[0].alt_tag).to eq("people standing on corner road near concrete buildings during daytime")
-      expect(pictures[0].url).to eq("https://images.unsplash.com/photo-1518105779142-d975f22f1b0a?crop=entropy&cs=tinysrgb&fm=jpg&ixid=Mnw0MTg4NjN8MHwxfHNlYXJjaHwxfHxNZXhpY298ZW58MHx8fHwxNjc4MDU3MzM2&ixlib=rb-4.0.3&q=80")
+      expect(pictures[0].url).to eq("https://images.unsplash.com/photo-1518105779142-d975f22f1b0a?crop=entropy&cs=tinysrgb&fm=jpg&ixid=Mnw0MTkyOTN8MHwxfHNlYXJjaHwxfHxNZXhpY298ZW58MHx8fHwxNjc4MTUzODg5&ixlib=rb-4.0.3&q=80")
     end
   end
 end
